7775403e695f8dda80bd9c07b96229d42978f18d
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-15 19:52:02 +0200

    commit

---
+++ src/main/java/com/tpe/domain/Book.java
@@ -0,0 +1,29 @@
+package com.tpe.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_book")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    private String author;
+
+    private String publishDate;
+}
